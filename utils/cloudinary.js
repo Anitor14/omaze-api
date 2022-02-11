8bbdc48d1f5e76daa47b6bbabb224e9f3a11c155
Anitor <anitorabraham@gmail.com>
2022-02-11 15:04:25 +0100

    updates made on api

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -19,4 +19,4 @@ exports.multiCloudinaryUpload = (file, folder) => {
 			folder: folder,
 		})
 	})
-}
+} 
--- utils/firebaseDb.js
+++
@@ -1,7 +0,0 @@
-// const firebase = require('firebase');
-const firebase = require('firebase-admin');
-
-const { FIREBASE_CONFIG } = require("./app");
-const db = firebase.initializeApp(FIREBASE_CONFIG);
-
-module.exports = db;
